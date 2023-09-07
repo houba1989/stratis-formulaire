13be5d309cf6bd221c18fe4adec42cd810c139e4
houba1989 <iheb.oxydev1@gmail.com>
2023-09-07 03:02:50 +0100

    correction commentaire

--- stratis-formulaire.php
+++ stratis-formulaire.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Plugin Name: Stratis Formulaire
  * Plugin URI: https://www.stratisworldwide.com/
@@ -9,47 +8,47 @@
  * Author URI: https://www.facebook.com/iheb.grayaa.01/
  **/
 
-// Définir une constante pour le chemin du fichier du plugin
+// Define a constant for the plugin file path
 if (!defined('STRATIS_FORM_PLUGIN_FILE')) {
     define('STRATIS_FORM_PLUGIN_FILE', __FILE__);
 }
 
-// Enfile le fichier CSS du plugin
-function STRATIS_assets()
+// Enqueue the plugin's CSS file
+function stratis_assets()
 {
-    wp_enqueue_style('style', plugin_dir_url(STRATIS_FORM_PLUGIN_FILE) . '/assets/css/style.css', false);
+    wp_enqueue_style('stratis-style', plugin_dir_url(STRATIS_FORM_PLUGIN_FILE) . '/assets/css/style.css', array(), '1.0.0');
 }
-add_action('wp_enqueue_scripts', 'STRATIS_assets');
+add_action('wp_enqueue_scripts', 'stratis_assets');
 
-// Définit le shortcode [stratis-formulaire]
+// Define the shortcode [stratis-formulaire]
 function formulaire_stratis()
 {
     ob_start();
 
-    // Vérifie si le formulaire a été soumis
+    // Check if the form has been submitted
     if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['stratis_submit'])) {
         $titre = sanitize_text_field($_POST['stratis_titre']);
         $texte = sanitize_text_field($_POST['stratis_texte']);
 
-        // Vérifie si les champs titre et texte sont vides
+        // Check if title and text fields are empty
         if (empty($titre) || empty($texte)) {
             echo '<div class="error">Les champs "Titre" et "Texte" sont obligatoires.</div>';
         } else {
-            // Vérifie si un article avec le même titre existe déjà
+            // Check if a post with the same title already exists
             $existing_post = get_page_by_title($titre, OBJECT, 'post');
             if ($existing_post === null) {
-                // Insère un nouvel article
+                // Insert a new post
                 $post_id = wp_insert_post(array(
                     'post_title' => $titre,
                     'post_content' => $texte,
-                    'post_status' => 'draft',
+                    'post_status' => 'draft', // You may want to change this to 'publish'
                     'post_type' => 'post',
                 ));
 
-                // Envoie une notification par e-mail
+                // Send an email notification
                 $to = get_option('admin_email');
-                $subject = 'Nouveau article créé';
-                $message = "Un nouveau article a été créé avec le titre : $titre\n\n$texte";
+                $subject = 'Nouvel article créé';
+                $message = "Un nouvel article a été créé avec le titre : $titre\n\n$texte";
                 wp_mail($to, $subject, $message);
 
                 echo '<div class="success">Article créé avec succès !</div>';
@@ -59,18 +58,18 @@ function formulaire_stratis()
         }
     }
 
-    // Affiche le formulaire
+    // Display the form
     ?>
     <form method="post">
         <div class="form-input">
             <label for="stratis_titre">Titre</label>
-            <input type="text" class="titre" id="stratis_titre" name="stratis_titre" required><br>
+            <input type="text" class="stratis_titre" id="stratis_titre" name="stratis_titre" required><br>
         </div>
         <div class="form-input">
             <label for="stratis_texte">Texte</label>
             <textarea class="stratis_texte" id="stratis_texte" name="stratis_texte" required></textarea><br>
         </div>
-        <input class="btn-startis" type="submit" name="stratis_submit" value="Envoyer">
+        <input type="submit" class="btn-stratis" name="stratis_submit" value="Envoyer">
     </form>
     <?php
 
@@ -78,16 +77,16 @@ function formulaire_stratis()
 }
 add_shortcode('stratis-formulaire', 'formulaire_stratis');
 
-// Fonction pour créer automatiquement une page lorsque le plugin est activé
-function creer_page_automatique()
+// Function to automatically create a page when the plugin is activated
+function create_auto_page()
 {
-    $page_title = 'Formulaire de création un post';
+    $page_title = 'Formulaire de création d\'un post';
     $page_content = '[stratis-formulaire]';
 
-    // Vérifie si la page existe déjà
+    // Check if the page already exists
     $page = get_page_by_title($page_title);
 
-    // Si la page n'existe pas, la crée
+    // If the page doesn't exist, create it
     if (!$page) {
         $page = array(
             'post_title' => $page_title,
@@ -100,5 +99,5 @@ function creer_page_automatique()
     }
 }
 
-// Enregistre le crochet d'activation pour créer la page
-register_activation_hook(__FILE__, 'creer_page_automatique');
+// Register the activation hook to create the page
+register_activation_hook(__FILE__, 'create_auto_page');
